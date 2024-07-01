cf25a65d2c38e45c5da9627ef5a177c0651620b5
John Versus <johnvs.nagendra@gmail.com>
2024-07-01 22:06:01 +0530

    NEXT_PUBLIC_SERVER_URL in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SERVER_URL:
+      process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+};
 
 export default nextConfig;
