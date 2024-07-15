05c83c17d2c746a36ccab3d3a25fdebb2ad7688d
John Versus <johnvs.nagendra@gmail.com>
2024-07-15 11:45:30 +0530

    openapi version change

--- app/api/swagger/swagger.ts
+++ app/api/swagger/swagger.ts
@@ -17,7 +17,7 @@ export const OpenAPIV1 = generateOpenApi(
       version: "1.0.0",
       description: "API documentation for the BookStore demo project",
     },
-    openapi: "3.1.0",
+    openapi: "3.0.3",
     servers: [
       {
         url: serverUrl,
